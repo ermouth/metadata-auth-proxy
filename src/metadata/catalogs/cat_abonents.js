62e036c48fac465fa6b7a974826cdd3f131c6d03
unpete <info@oknosoft.ru>
2024-04-12 15:28:08 +0500

    отладка индексера

--- src/metadata/catalogs/cat_abonents.js
+++ src/metadata/catalogs/cat_abonents.js
@@ -7,8 +7,8 @@ exports.CatAbonents = class CatAbonents extends Object {
    * @return {PouchDB}
    */
   db(cachable) {
-    const {job_prm: {server}, adapters: {pouch}} = this._manager._owner.$p;
-    return server.abonents.length < 2 && !server.single_db ? pouch.remote[cachable] : this.server.db(this, cachable);
+    const {job_prm: {server, zone}, adapters: {pouch}} = this._manager._owner.$p;
+    return server.abonents.length < 2 && !server.single_db && zone == this.id ? pouch.remote[cachable] : this.server.db(this, cachable);
   }
 
   /**
